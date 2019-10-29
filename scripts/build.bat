54d0923ed605cbaaf75d2b701c3123c8aa473447
Dmitriy Finozhenok <dmitry.finozhenok@aspose.com>
2019-10-29 16:16:53 +0900

    Use dotnet restore always

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,2 +1,4 @@
-c:\build\.nuget\NuGet.exe restore c:\build\Aspose.Words.Cloud.Sdk.sln
+dotnet restore c:\build\Aspose.Words.Cloud.Sdk\Aspose.Words.Cloud.Sdk.csproj
+dotnet restore c:\build\Aspose.Words.Cloud.Sdk.Tests\Aspose.Words.Cloud.Sdk.Tests.csproj
+dotnet restore c:\build\Aspose.Words.Cloud.Sdk.BddTests\Aspose.Words.Cloud.Sdk.BddTests.csproj
 msbuild c:\build\Aspose.Words.Cloud.Sdk.sln
